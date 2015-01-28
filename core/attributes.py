a28df16457f729578b6b4c6e303bb53608a857ee
Michael Fessenden <michael.fessenden@gmail.com>
2015-01-28 07:49:47 -0500

    - update Atributes classes

--- core/attributes.py
+++ core/attributes.py
@@ -1,85 +1,170 @@
 #!/X/tools/binlinux/xpython
 import simplejson as json
+import uuid
+from SceneGraph import util
 
 
-__all__ = ['StringAttribute', 'IntegerAttribute', 'FloatAttribute']
+__all__  = [StringAttribute, IntegerAttribute, FloatAttribute]
 
 
-class AttributeBase(object):
-    """
-    Base attribute class
-    """
-    def __init__(self, parent=None, name=None, value=None, index=0):
+class Attribute(object):
+
+    def __init__(name, value=None, parent=None, index=0):
+
+        self._name            = name        
+        self._parent          = parent
+
+        # value & overrides
+        self._value           = value
+        self._value_override  = None        # secondary "override" value
+        self._has_override    = False       # signifies that the value has an override
+
+        # indexing
+        self._index           = index       # internal index (for recalling in order)
+        self._type            = None        # attribute type (int, float, string, etc.)   
         
-        self._parent        = parent                # parent object (Node)
-        self._name          = name                  # attribute name
-        self._value         = {'default':value}     # attribute value (allows for overrides)
-        
-        self._index         = index                 # internal index (for recalling in order)
-        self._type          = None                  # attribute type (int, float, string, etc.)   
-        
-        self.is_locked      = False                 # attribute cannot be changed
-        self.is_private     = False                 # attribute is private & hidden from the ui  
-    
+        self._is_locked       = False       # attribute cannot be changed
+        self._is_private      = False       # attribute is private & hidden from the ui  
+
+
     def __repr__(self):
-        return str(self.data())
-    
-    def __lt__(self, other):
-        return self.getValue() < other
-    
-    def data(self, type='default'):
-        return {self.name:self.getValue(type)}
-    
+        if self.value is not None:
+            return '"%s" : "%s"' % (self.name, self.value)
+        return '"%s" : None' % self.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return (self.name) == (other.name)
+
     @property
     def name(self):
-        return self._name
-    
+        return self.name
+
     @name.setter
     def name(self, name=None):
-        self._name = name
-        return self._name
-    
+        if self._name != name:
+            self._name = name
+        return self.name
+
     @property
     def nice_name(self):
-        return self._name.title()
-    
-    def getValue(self, type='default'):
+        return util.clean_name(self.name)
+
+    @property
+    def value(self):
         """
-        Return a value of the given type
+        Returns the current value. If the attribute has an override, 
+        return that instead.
         """
-        return self._value.get(type, None)
-    
-    def setValue(self, type='default', value=None):
+        if self._has_override:
+            return self._value_override
+        return self._value
+
+    @value.setter
+    def value(self, value=None):
         """
-        Set a value of the given type
+        Set the current value - If the attribute currently 
+        has an override, set that.
         """
-        self._value.update(**{type:value})
-        return self._value.get(type, None)
+        if not self.is_locked:
+            if self._has_override:
+                return self._value_override = value
+            else:
+                self._value = value
+        return self.value
+
+    @property
+    def attribute_type(self):
+        return self._type
+
+    #- Overrides ----
+    def addOverride(self, value=None):
+        """
+        Override the attribute value with another.
+        """
+        if not self.is_locked:
+            self._has_override = True
+            if self._value_override != value:
+                self._value_override = value
+        return self.value
+
+    def clearOverride(self):
+        """
+        Remove the override flag - but stash any override 
+        value for future use.
+        """
+        if not self._is_locked:
+            self._has_override = False
+
+    #- Locking ---
+    @property
+    def is_locked(self):
+        """
+        Returns the current lock state of the attribute.
+        """
+        return self._is_locked
+
+    def lock(self):
+        """
+        Lock the attribute. If successful, returns true.
+        """
+        if self._is_locked is not True:
+            self._is_locked = True
+            return True
+        return False
+
+    def unlock(self):
+        """
+        Unock the attribute. If successful, returns true.
+        """
+        if self._is_locked is not False:
+            self._is_locked = False
+            return True
+        return False
 
     
-class StringAttribute(AttributeBase):
-    def __init__(self, parent=None, name=None, value=None):
-        super(StringAttribute, self).__init__(parent, name, value)        
-        self._type          = 'string'
-        
-    def setValue(self, type='default', value=None):
+class StringAttribute(Attribute):
+    def __init__(self, name, value=None, parent=None, **kwargs):
+        super(StringAttribute, self).__init__(name, value=value, parent=parent, **kwargs)        
+        self._type = 'string'
+    
+    @property 
+    def value(self):
+        return str(self.value)
+
+    @value.setter
+    def value(self, value=None):
         if value: value = str(value)
-        return super(StringAttribute, self).setValue(value)
+            self.value = value
 
 
-class IntegerAttribute(AttributeBase):
-    def __init__(self, parent=None, name=None, value=None):
-        super(IntegerAttribute, self).__init__(parent, name, value)        
-        self._type          = 'int'
+class IntegerAttribute(Attribute):
+    def __init__(self, name, value=None, parent=None, **kwargs):
+        super(IntegerAttribute, self).__init__(name, value=value, parent=parent, **kwargs)        
+        self._type  = 'int'
 
-    def setValue(self, type='default', value=None):
+    @property 
+    def value(self):
+        return int(self.value)
+
+    @value.setter
+    def value(self, value=None):
         if value: value = int(value)
-        return super(IntegerAttribute, self).setValue(value)
+            self.value = value
 
-
-class FloatAttribute(AttributeBase):
-    def __init__(self, parent=None, name=None, value=None):
-        super(FloatAttribute, self).__init__(parent, name, value)        
-        self._type          = 'float'
         
+class FloatAttribute(Attribute):
+    def __init__(self, name, value=None, parent=None, **kwargs):
+        super(FloatAttribute, self).__init__(name, value=value, parent=parent, **kwargs)        
+        self._type  = 'float'
 
+    @property 
+    def value(self):
+        return float(self.value)
+
+    @value.setter
+    def value(self, value=None):
+        if value: value = float(value)
+            self.value = value
--- core/nodes.py
+++ core/nodes.py
@@ -755,24 +755,38 @@ class LineClass(QtGui.QGraphicsLineItem):
 
 #- Testing -----
 class SimpleNode(QtGui.QGraphicsItem):
-    Type = QtGui.QGraphicsItem.UserType + 3
-    def __init__(self):
+    
+    Type                = QtGui.QGraphicsItem.UserType + 3
+    clickedSignal       = QtCore.Signal(QtCore.QObject)
+    nodeCreatedInScene  = QtCore.Signal()
+    nodeChanged         = QtCore.Signal(bool)
+
+    def __init__(self, name='Node1', width=100, height=175, font='Consolas'):
         QtGui.QGraphicsItem.__init__(self)
         
-        self.name = 'MyNode'
-        self.width = 100
-        self.height = 200
-        self.buffer = 5
-        self.color = [180, 180, 180]
-
+        self.name            = name
+        self.width           = width
+        self.height          = height
+        
+        # buffers
+        self.bufferX         = 3
+        self.bufferY         = 3
+        self.color           = [180, 180, 180]
+        
+        self.label           = QtGui.QGraphicsSimpleTextItem(parent=self)
+        self._font_family    = font
+        self._font_size      = 10
+        self._font_bold      = False
+        self._font_italic    = False
+        self.font            = QtGui.QFont(self._font_family)
+        self._is_node        = True
+        
         self.setFlags(QtGui.QGraphicsItem.ItemIsSelectable | QtGui.QGraphicsItem.ItemIsMovable | QtGui.QGraphicsItem.ItemIsFocusable)
-        self.setCachingEnabled(True)
         self.setAcceptHoverEvents(True)
 
         self.setAcceptedMouseButtons(QtCore.Qt.LeftButton)
         self.setCacheMode(self.DeviceCoordinateCache)
         self.setZValue(-1)        
-        self.radius = 10
 
     def type(self):
         """
@@ -785,17 +799,46 @@ class SimpleNode(QtGui.QGraphicsItem):
         Defines the clickable hit-box.  Simply returns a rectangle instead of
         a rounded rectangle for speed purposes.
         """
-        return QtCore.QRectF(-self.width/2  - self.buffer,  -self.height/2 - self.buffer,
-                              self.width  + 3 + self.buffer, self.height + 3 + self.buffer)
+        return QtCore.QRectF(-self.width/2  - self.bufferX,  -self.height/2 - self.bufferY,
+                              self.width  + 3 + self.bufferX, self.height + 3 + self.bufferY)
     
+    # Label formatting -----
+    def setLabelItalic(self, val=False):
+        self._font_italic = val
 
+    def setLabelBold(self, val=False):
+        self._font_bold = val
+
+    def buildNodeLabel(self):
+        self.label.setX(-(self.width/2 - self.bufferX))
+        self.label.setY(-(self.height/2 - self.bufferY))
+        self.font = QtGui.QFont(self._font_family)
+        self.font.setPointSize(self._font_size)
+        self.font.setBold(self._font_bold)
+        self.font.setItalic(self._font_italic)
+        self.label.setFont(self.font)
+        self.label.setText(self.name)
+    
+    def getLabelLine(self):
+        p1 = self.boundingRect().topLeft()
+        p1.setY(p1.y() + self.bufferY*8)
+        p2 = self.boundingRect().topRight()
+        p2.setY(p2.y() + self.bufferY*8)
+        return QtCore.QLineF(p1, p2)
+    
     def paint(self, painter, option, widget):
         """
-        Draw the nub.
+        Draw the node.
         """
+        # label & line
+        self.buildNodeLabel()        
+        label_line = self.getLabelLine()
+       
         painter.setBrush(QtGui.QBrush(QtGui.QColor(*self.color)))
         painter.setPen(QtGui.QPen(QtCore.Qt.black, 0))
         painter.drawRect(self.boundingRect())
+        painter.drawLine(label_line)
+        #painter.drawText(self.boundingRect().x()+self.buffer, self.boundingRect().y()+self.buffer, self.name)
 
 
 class MyLine(QtGui.QGraphicsLineItem):
